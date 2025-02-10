054997f317ca4c843369ae4ce2f5ab7132393b6e
mohcinsarrar <43006742+mohcinsarrar@users.noreply.github.com>
2025-02-10 19:34:27 +0100

    add

--- app/Http/Controllers/users/SubscriptionController.php
+++ app/Http/Controllers/users/SubscriptionController.php
@@ -14,11 +14,21 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\SubscriptionEmail;
 use LaravelCountries;
 use Laravel\Cashier\Events\OrderInvoiceAvailable;
+use Illuminate\Support\Facades\Http;
+
 
 use Carbon\Carbon;
 
 class SubscriptionController extends Controller
 {
+
+  private $supported_currencies = [
+    "AED", "AUD", "BGN", "BRL", "CAD", "CHF", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "ILS", "ISK", "JPY", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TWD", "USD", "ZAR"
+  ];
+
+  private $default_currency = 'USD';
+
+
   public function index()
   {
     $user = Auth::user();
@@ -31,6 +41,53 @@ class SubscriptionController extends Controller
     
   }
 
+  private function get_vat($country, $amount){
+    try {
+        $vatApiKey = "967d4f730997f2425bd85a2824f99c41";
+        $vatResponse = Http::get("http://apilayer.net/api/price", [
+            'access_key' => $vatApiKey,
+            'amount' => $amount,
+            'country_code' => $country,
+        ]);
+
+        $response = $vatResponse->json();
+        
+        if ($response['success'] != true) {
+            throw new \Exception("API request failed");
+        } 
+
+        return $vatResponse->json()['price_incl_vat'];
+
+    } catch (\Exception $e) {
+        return null;
+    }
+    
+  }
+
+  private function get_currency($currency){
+    try {
+        $curencyApiKey = "f9b3e81a943ad806539729cfe97b5d70";
+        $curencyResponse = Http::get("http://apilayer.net/api/live", [
+            'access_key' => $curencyApiKey,
+            'source' => $this->default_currency,
+            'currencies' => $currency,
+            'format' => '1'
+        ]);
+
+        $response = $curencyResponse->json();
+
+        if ($response['success'] != true) {
+            throw new \Exception("API request failed");
+        } 
+
+        $firstValue = reset($response['quotes']);
+        return $firstValue;
+    } catch (\Exception $e){
+        return null;
+    }
+    
+  }
+
 
   private function getObjectById($array, $id) {
     $result = array_filter($array, function($object) use ($id) {
@@ -40,6 +97,8 @@ class SubscriptionController extends Controller
     // Return the first match (or null if not found)
     return $result ? array_values($result)[0] : null;
     }
+
+    
 
 
   public function payment(Request $request, $id)
@@ -58,25 +117,73 @@ class SubscriptionController extends Controller
                 $upgrade = true;
             }
         }
-        /*
+
+        $amount = (string)$product->price;
+
+        // get user country 
+        $countries = LaravelCountries::getCountries()->getData();
+        //dd($user->country);
+        $user_country = $this->getObjectById($countries, 75);
+
+        $user_country_code = $user_country->iso2;
+        $user_currency = $user_country->currency;
+
+        // get price include vat_rate
+        $price_incl_vat = $this->get_vat($user_country_code, $amount);
+        if($price_incl_vat == null){
+            return redirect()->route('users.subscription.index')->with('error','your payment has not been completed, please try again');
+        }
+
+        // get currency exchange if user has a supported currency
+
+        if(!in_array($user_currency, $this->supported_currencies)){
+            $user_currency = $this->default_currency;
+        }
+
+        $currency_rate = 0;
+        if($user_currency != $this->default_currency){
+            $currency_rate = $this->get_currency($user_currency);
+            if($currency_rate == null){
+                return redirect()->route('users.subscription.index')->with('error','your payment has not been completed, please try again');
+            }
+        }
+        
+        // calculate the final price with vat and currency exchange
+        
+        $final_price = $price_incl_vat;
+        if($currency_rate != 0){
+            $final_price = $price_incl_vat * $currency_rate;
+        }
+
+        
+        $final_price = number_format($final_price, 2);
+
         $payment = Mollie::api()->payments->create([
             "amount" => [
-                "currency" => "USD",
-                "value" => (string)$product->price, // You must send the correct number of decimals, thus we enforce the use of strings
+                "currency" => $user_currency,
+                "value" => (string)$final_price, // You must send the correct number of decimals, thus we enforce the use of strings
             ],
             "description" => $product->name,
-            "redirectUrl" => route('users.subscription.success'),
-            //"webhookUrl" => route('webhooks.mollie'),
-            "metadata" => [
-                "order_id" => time(),
+            "billingAddress" => [
+                "givenName" => $user->firstname,  // First name
+                "familyName" => $user->lastname,  // Last name
+                "streetAndNumber" => $user->address,  // Street address
+                "city" => $user->city,  // City
+                "postalCode" => "12345",  // Postal code
+                "country" => $user_country_code,  // Country code (e.g., 'US', 'NL')
+                "email" => $user->email,
             ],
+            "redirectUrl" => route('users.subscription.success'),
+            "cancelUrl" => route('users.subscription.success'),
+            //"webhookUrl" => route('webhooks.mollie'),
+
         ]);
-        */
+
         
-        $countries = LaravelCountries::getCountries()->getData();
-        $user_country = $this->getObjectById($countries, $user->country)->iso2;
+        
 
         // Create Order using Mollie's Orders API
+        /*
         $order = Mollie::api()->orders->create([
             "amount" => [
                 "currency" => "USD",
@@ -121,28 +228,33 @@ class SubscriptionController extends Controller
                 "product_id" => $product->id
             ],
         ]);
-
-        session()->put('orderId', $order->id);
+        */
+        //session()->put('orderId', $order->id);
+        session()->put('paymentId', $payment->id);
         session()->put('productId', $product->id);
     
         // redirect customer to Mollie checkout page
         
         
-        return redirect($order->getCheckoutUrl(), 303);
+        //return redirect($order->getCheckoutUrl(), 303);
+        return redirect($payment->getCheckoutUrl(), 303);
     }
 
     public function success(Request $request)
     {
-        $orderId = session()->get('orderId');
+        //$orderId = session()->get('orderId');
+        $paymentId = session()->get('paymentId');
         $productId = session()->get('productId');
 
         $product = Product::findOrFail($productId);
 
-        $order = Mollie::api()->orders->get($orderId);
+        //$order = Mollie::api()->orders->get($orderId);
 
-        //$payment = Mollie::api()->payments->get($paymentId);
-        //if($payment->isPaid())
-        if ($order->isPaid() || $order->isAuthorized()) 
+        $payment = Mollie::api()->payments->get($paymentId);
+        // Simulate failed status
+
+        if($payment->isPaid())
+        //if ($order->isPaid() || $order->isAuthorized()) 
         {
             $user = Auth::user();
             $upgrade = false;
@@ -163,13 +275,16 @@ class SubscriptionController extends Controller
             }
 
             $paymentObj = new Payment();
-            $paymentObj->payment_id = $orderId;
+            $paymentObj->payment_id = $paymentId;
+            //$paymentObj->payment_id = $orderId;
             $paymentObj->user_id = $user->id;
             $paymentObj->product_id = $productId;
-            $paymentObj->currency = $order->amount->currency;
+            //$paymentObj->currency = $order->amount->currency;
+            $paymentObj->currency = $payment->amount->currency;
             $paymentObj->price = $product->price;
             $paymentObj->payment_status = "Completed";
-            $paymentObj->payment_method = $order->method;
+            //$paymentObj->payment_method = $order->method;
+            $paymentObj->payment_method = $payment->method;
             $paymentObj->created_at = Carbon::now();
             $paymentObj->save();
         
@@ -196,9 +311,9 @@ class SubscriptionController extends Controller
             $fantree->print_number = 0;
             $fantree->save();
             
-            return redirect()->route('users.subscription.index');
+            return redirect()->route('users.subscription.index')->with('success','your payment has been completed');
         } else {
-            return redirect()->route('users.subscription.index');
+            return redirect()->route('users.subscription.index')->with('error','your payment has not been completed, please try again');
         }
     }
 
